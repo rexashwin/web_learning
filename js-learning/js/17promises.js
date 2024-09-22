ceaff50031587cc548f9ac15d4044cb450f8a469
Ashwin Raj <rajashwin1163@gmail.com>
2024-09-22 08:27:54 +0530

    js - Promises

--- js-learning/js/17promises.js
+++ js-learning/js/17promises.js
@@ -1,2 +1,8 @@
 // creating Promises
-const promiseOne = new Promise(function(resolve, reject){});
+const promiseOne = new Promise(function(resolve, reject){
+    // do any async task
+    // DB calls, cyrptography, network calls
+    setTimeout(function(){
+        console.log("promiseOne - Async Task");
+    }, 1000);
+});
