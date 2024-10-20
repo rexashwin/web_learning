d1d879d23f8fa68142dfccf22a3903bf6d8414db
Ashwin Raj <rajashwin1163@gmail.com>
2024-10-20 07:43:41 +0530

    js - Promises async/await fix: response to json

--- js-learning/js/17promises.js
+++ js-learning/js/17promises.js
@@ -95,8 +95,8 @@ consumePromiseFive()
 
 async function getAllUser(){
     const response = await fetch('https://api.github.com/users/rexashwin');
-    console.log(response);
-    const data = response.json()
+    // console.log(response);
+    const data = await response.json()
     console.log(data);
 }
 getAllUser();
