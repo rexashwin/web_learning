87fc35ce32117c11a67a83dede138b0601ebaf5c
Ashwin Raj <rajashwin1163@gmail.com>
2024-10-23 07:52:19 +0530

    js - Promises async/await

--- js-learning/js/17promises.js
+++ js-learning/js/17promises.js
@@ -106,6 +106,8 @@ async function getAllUser(){
     try{
         const response = await fetch('https://api.github.com/users/rexashwin');
         console.log(response);
+        const data = await response.json()
+        console.log(data);
     }
     catch(error){
         console.log(`E: ${error}`);
