0df6cbe9403f051a91b9ee8014d510365c87a0ff
Ashwin Raj <rajashwin1163@gmail.com>
2024-10-27 12:27:40 +0530

    js promises2: passing argument to a function

---
+++ js-learning/js/17promises2.js
@@ -0,0 +1,14 @@
+import fetch from "node-fetch";
+
+const delayedGreeting = new Promise(function (resolve, reject) {
+    if (timeout > 0) {
+        setTimeout(() => {
+            resolve(`Hello ${name}!`)
+        }, timeout);
+    } else {
+        reject(`Invalid delay time`)
+    }
+})
+delayedGreeting("Alia", 2000)
+    .then((msg) => console.log(msg))
+    .catch((msg) => console.log(msg))
