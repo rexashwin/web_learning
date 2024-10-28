5e9a744ab8f1e959f7d7243c0437224c533c4eab
Ashwin Raj <rajashwin1163@gmail.com>
2024-10-28 12:30:21 +0530

    js promises2: fix: passing argument to a function

--- js-learning/js/17promises2.js
+++ js-learning/js/17promises2.js
@@ -1,14 +1,17 @@
 import fetch from "node-fetch";
 
-const delayedGreeting = new Promise(function (name, timeout, resolve, reject) {
-    if (timeout > 0) {
-        setTimeout(() => {
-            resolve(`Hello ${name}!`)
-        }, timeout);
-    } else {
-        reject(`Invalid delay time`)
-    }
-})
+const delayedGreeting = function (name, timeout) {
+    return new Promise(function (resolve, reject) {
+        if (timeout > 0) {
+            setTimeout(() => {
+                resolve(`Hello ${name}!`)
+            }, timeout);
+        } else {
+            reject(`Invalid delay time`)
+        }
+    })
+}
+
 delayedGreeting("Alia", 2000)
     .then((msg) => console.log(msg))
     .catch((msg) => console.log(msg))
